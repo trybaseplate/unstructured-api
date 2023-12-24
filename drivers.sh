25d7c3bc1cdaf6c38ec178c71fa9962cad0119c2
Andrew Luo <andrew@trybaseplate.io>
2023-12-23 23:19:13 -1000

    Create drivers.sh

---
+++ drivers.sh
@@ -0,0 +1,5 @@
+DRIVER=535.54.03
+
+sudo yum install gcc10 -y
+sudo wget -O /tmp/NVIDIA-Linux-driver.run "https://us.download.nvidia.com/tesla/${DRIVER}/NVIDIA-Linux-x86_64-${DRIVER}.run"
+sudo CC=gcc10-cc sh /tmp/NVIDIA-Linux-driver.run -q -a --ui=none
