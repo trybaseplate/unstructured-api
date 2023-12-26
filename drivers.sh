be5bb51635410207f55f7b7a122e445b4376b09f
Andrew Luo <andrew@trybaseplate.io>
2023-12-26 13:28:41 -0800

    Update drivers.sh

--- drivers.sh
+++ drivers.sh
@@ -2,5 +2,6 @@ DRIVER=535.54.03
 
 yum update
 yum install sudo -y
-yum install gcc10 -y
-wget -O /tmp/NVIDIA-Linux-driver.run "https://us.download.nvidia.com/tesla/${DRIVER}/NVIDIA-Linux-x86_64-${DRIVER}.run" CC=gcc10-cc sh /tmp/NVIDIA-Linux-driver.run -q -a --ui=none
+sudo yum install gcc10 -y
+sudo wget -O /tmp/NVIDIA-Linux-driver.run "https://us.download.nvidia.com/tesla/${DRIVER}/NVIDIA-Linux-x86_64-${DRIVER}.run"
+sudo CC=gcc10-cc sh /tmp/NVIDIA-Linux-driver.run -q -a --ui=none
