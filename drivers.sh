56e4f0e462352c5a516ac95450193c810d26b6fc
Andrew Luo <andrew@trybaseplate.io>
2024-01-12 15:52:13 -0500

    Update drivers.sh

--- drivers.sh
+++ drivers.sh
@@ -1,1 +1,1 @@
-docker run -p 80:8000 --gpus all nvidia/cuda:11.6.2-base-ubuntu20.04 nvidia-smi --rm --name unstructured-gpu unstructured-gpu
+docker run -p 80:8000 --gpus=all --rm --name unstructured-gpu unstructured-gpu
